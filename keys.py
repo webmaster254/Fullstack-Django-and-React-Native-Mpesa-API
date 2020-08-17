95c6941bf331b6256a4413486cb242addf61a6fa
webmaster254 <josephm280@gmail.com>
2020-08-17 23:52:10 +0100

    added test nsisdn

---
+++ keys.py
@@ -0,0 +1,7 @@
+Business_shortCode="174379"
+Phone_number="254791671303"
+lipa_na_mpesa_passkey="bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key="JCixBOLdTjSTdjlGJGtIdiPZA5r8uuzh"
+consumer_secret="M0pWAzA61Jurp0xY"
+shortCode="601426"
+msisdn="254708374149"
