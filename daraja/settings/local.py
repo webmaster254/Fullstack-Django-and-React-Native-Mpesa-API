8e8ffb749af5e953683bfca4722e06ec7c6204a7
webmaster254 <josephm280@gmail.com>
2020-08-21 02:52:20 +0100

    changed database

--- daraja/settings/local.py
+++ daraja/settings/local.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'daraja.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'daraja',
+        'USER': 'postgres',
+        'PASSWORD': 'joseph2800',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
